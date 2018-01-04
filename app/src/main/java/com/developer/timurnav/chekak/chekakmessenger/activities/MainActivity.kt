df04a795a8edbc3678062372a89e723e228e90ef
timurnav <timurnav@yandex-team.ru>
2018-01-04 18:26:44 +0300

    redirect to dashboard if already authorized

--- app/src/main/java/com/developer/timurnav/chekak/chekakmessenger/activities/MainActivity.kt
+++ app/src/main/java/com/developer/timurnav/chekak/chekakmessenger/activities/MainActivity.kt
@@ -4,14 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.developer.timurnav.chekak.chekakmessenger.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mListener: FirebaseAuth.AuthStateListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+        mListener = FirebaseAuth.AuthStateListener {
+            it.currentUser?.let {
+                startActivity(Intent(this, DashboardActivity::class.java))
+                finish()
+            }
+        }
 
         createAccountButton.setOnClickListener {
             startActivity(Intent(this, CreateAccountActivity::class.java))
@@ -20,6 +32,15 @@ class MainActivity : AppCompatActivity() {
         loginExistingAccountButton.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
+    }
 
+    override fun onStart() {
+        super.onStart()
+        mAuth.addAuthStateListener(mListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mAuth.removeAuthStateListener(mListener)
     }
 }
